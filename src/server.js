052d262c229499b624517b250f5206c7e4301dbb
ncp204 <19130172@st.hcmuaf.edu.vn>
2023-07-19 20:05:18 +0700

    update

--- src/server.js
+++ src/server.js
@@ -1,13 +1,24 @@
 require("dotenv").config();
+import bodyParser from "body-parser";
 import express from 'express';
-import configViewEngine from './configs/viewEngine';
+import configViewEngine from './config/viewEngine';
 import initWebRoutes from './routes/web';
+import connection from "./config/connectDB";
 
 const app = express();
+
 const PORT = process.env.PORT || 8083;
+
+
+// config body-parser
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
 // config view engine
 configViewEngine(app);
+
+// test connection db
+connection();
 
 // init web route
 initWebRoutes(app);
