d2428578142fb74b4afb8201a7c382f089f2f5a5
ncp204 <19130172@st.hcmuaf.edu.vn>
2023-07-21 09:37:43 +0700

    Use JWT

--- src/server.js
+++ src/server.js
@@ -4,11 +4,31 @@ import express from 'express';
 import configViewEngine from './config/viewEngine';
 import initWebRoutes from './routes/web';
 import connection from "./config/connectDB";
+import JWTAction from './middleware/JWTAction'
 
 const app = express();
 
 const PORT = process.env.PORT || 8083;
 
+// Add header berfore the routes are defined
+app.use(function (req, res, next) {
+
+    // Website you wish to allow to connect
+    res.setHeader('Access-Control-Allow-Origin', process.env.REACT_URL);
+
+    // Request methods you wish to allow
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+
+    // Request headers you wish to allow
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested, content-type');
+
+    // Set to true if you need the webste to include cookies in the requests sent
+    // to the API (e.g. in case you use sessions)
+    res.setHeader('Access-Control-Allow-Credetials', true);
+
+    // Pass to next layer of middleware
+    next();
+});
 
 // config body-parser
 app.use(bodyParser.json());
@@ -22,6 +42,8 @@ connection.testConnection();
 
 // init web route
 initWebRoutes(app);
+
+JWTAction.createJWT();
 
 app.listen(PORT, () => {
     console.log(">>> JWT Backend is running on the port: ", PORT);
